9fb59ff0078da8b19f3308f913fbc7a275ab4976
Vadym Markov <markov.vadym@gmail.com>
2016-03-15 10:56:28 +0100

    Rename example

---
+++ Example/FashionDemo/FashionDemo/Sources/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Fashion
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.whiteColor()
+  }
+}
+
